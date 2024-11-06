a1cabccfce00c63621c26723a1b6c37facde3ecc
Mandeep-Singh-6 <mandeepsingh6@academic.rrc.ca>
2024-11-05 18:07:58 -0600

    Finalized the header.php template.

--- header.php
+++ header.php
@@ -1,7 +1,25 @@
 <?php 
-require("connect.php");
+// Creating a query to get all the pages.
+
+    $query = "SELECT title FROM genericpages ORDER BY title DESC";
+
+    // Loading the query into the MySql server cache.
+    $statement = $db->prepare($query);
+
+    // Execute the query.
+    $statement->execute();
+
+
+    $results = $statement->fetchAll();
 
 ?>
-<ul>
-
-</ul>
+<div id = "header">
+    <h1>HealthMetLife</h1>
+    <nav>
+        <ul>
+            <?php foreach($results as $result): ?>
+                <li><a href = "?" ><?= $result['title'] ?></a></li>
+            <?php endforeach ?>
+        </ul>
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+require('connect.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HealthMetLife</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php require('header.php') ?>
+</body>
+</html>
